76271ed5d2297faaed0301cc8e61575ccfca8a0a
rezarajan <rezarajan@gmail.com>
2021-02-02 17:52:54 -0400

    feat: particle filter prediction step

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -63,6 +63,33 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
    *  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
    *  http://www.cplusplus.com/reference/random/default_random_engine/
    */
+  for(int i = 0; i < num_particles; ++i){
+    double x = particles[i].x;
+    double y = particles[i].y;
+    double theta = particles[i].theta;
+    // Assuming bicycle model
+    if(yaw_rate == 0){
+      x +=velocity*delta_t*cos(theta); 
+      y +=velocity*delta_t*sin(theta); 
+    }
+    else{
+      x += (velocity/yaw_rate)*(sin(particles[i].theta+yaw_rate*delta_t)-sin(particles[i].theta));
+      y += (velocity/yaw_rate)*(-cos(particles[i].theta+yaw_rate*delta_t)+cos(particles[i].theta));
+      theta += yaw_rate*delta_t;
+    }
+    std::default_random_engine gen; // Random engine for sampling
+    // Creating Normal (Gaussian) distributions
+    normal_distribution<double> dist_x(x, std_pos[0]);
+    normal_distribution<double> dist_y(y, std_pos[1]);
+    normal_distribution<double> dist_theta(theta, std_pos[2]);
+    
+    // Set the new particle predicted location
+    particles[i].x = dist_x(gen);
+    particles[i].y = dist_y(gen);
+    particles[i].theta = dist_theta(gen);
+  }
+
+
 
 }
 
